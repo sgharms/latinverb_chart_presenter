68ac6501c2e5af27c1640a278b3cc79878ebcc5b
Steven G. Harms <steven.harms@gmail.com>
2014-07-18 12:08:06 -0700

    Add test for SUM

--- test/chart_test.rb
+++ test/chart_test.rb
@@ -2,4 +2,8 @@ class ChartTest < Minitest::Test
   def test_chart
     Linguistics::Latin::Verb::LatinVerb::ChartPresenter.new(AFIRST).chart
   end
+
+  def test_chart_with_irregular_verb
+    Linguistics::Latin::Verb::LatinVerb::ChartPresenter.new(SUM).chart
+  end
 end
